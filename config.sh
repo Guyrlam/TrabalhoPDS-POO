5065f7f2e20d7f4b92574de8a7611aedb81683eb
guyrlam <guyrlamsilvaduque@gmail.com>
2024-06-18 12:27:57 -0300

    [Chore] - feat/change-interface: change CMakeList and config.sh

--- config.sh
+++ config.sh
@@ -16,7 +16,7 @@ apt install -y g++
 apt-get install -y build-essential
 snap install cmake --classic
 apt-get install -y gdb
-apt install libsdl2-dev libsdl2-image-dev pkg-config
+apt install libsdl2-dev libsdl2-image-dev pkg-config libsdl2-ttf-dev
 mkdir build           # Cria a pasta 'build'
 cd build              # Entra na pasta 'build'
 cmake ..              # Executa o CMake na pasta atual
